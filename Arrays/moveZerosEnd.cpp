567cf367d59c1763f798c4db4763fd63819675c5
Ikshit Dhiman <75236509+dhiman-64@users.noreply.github.com>
2023-07-31 19:37:05 +0530

    Day 2

--- Arrays/moveZerosEnd.cpp
+++ Arrays/moveZerosEnd.cpp
@@ -4,6 +4,55 @@ using namespace std;
 
 void moveZero(int arr[], int n)
 {
+    int j = -1;
+    for(int i=0 ; i<n ; i++)
+    {
+        if(arr[i] == 0)
+        {
+            j = i;
+            break;
+        }
+    }
+    if( j == -1 ) return;
+    
+    for(int i=j+1 ; i<n ; i++)
+    {
+        if(arr[i] != 0)
+        {
+            swap(arr[i] , arr[j]);
+            j++;
+        }
+    }
+}
+
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int arr[n];
+        for(int i=0 ; i<n ; i++)
+        {
+            cin>>arr[i];
+        }
+        // Function Calling
+        moveZero(arr,n);
+        for(int i=0 ; i<n ; i++)
+        {
+            cout<<arr[i]<<" ";
+        }
+    }
+}
+
+/*
+                BRUTE FORCE 
+
+void moveZero(int arr[], int n)
+{
     vector<int> temp;
     for(int i=0 ; i<n ; i++)
     {
@@ -46,3 +95,6 @@ int main()
         }
     }
 }
+
+
+*/
